0952ef226471fdfb8e057b63611a8b821ae5884e
Rifatur Rahman <123852806+Naruto-79@users.noreply.github.com>
2023-10-13 17:11:16 +0000

    operatores

---
+++ string.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//alert("hello")
+
+/*let a = 5
+let c = 10
+console.log(a-c);*/
+/*let name = "kamal"
+let symbol = {}
+let number = 145786
+let hulu = null
+let bool = false
+let birth = undefined
+let bigboy = BigInt
+
+console.log(typeof symbol);*/
+
+// let str1 = "hi"
+// let str2 = " there"
+
+// console.log(str1+str2);
+
+let a = 5
+a++
+
+console.log(a);
